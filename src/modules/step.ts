35f4c92e6e6f9ffef498a1b64cd36775982476c3
Kha <kha@desygner.com>
2021-02-06 23:26:07 +0000

    added images

--- src/modules/step.ts
+++ src/modules/step.ts
@@ -3,7 +3,9 @@ import {bindable} from 'aurelia-framework';
 export class step {
   @bindable stepNumber;
   @bindable stepText;
+  @bindable stepImg;
   @bindable stepTextDone;
+  @bindable stepImgDone;
 
   done: boolean = false;
 
